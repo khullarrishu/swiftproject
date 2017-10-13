c92d29499e7a87b7e34cb3aba83c2e5d384065b2
Aman Bhullar <amanbhullar@Amans-MacBook-Pro.local>
2017-10-13 13:47:16 -0400

    Initial Commit

---
+++ SwiftProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftProject
+//
+//  Created by Aman Bhullar on 2017-10-13.
+//  Copyright © 2017 Aman Bhullar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
